70cb0eaeb6b69b705ba0171d92a8afe29f81deaf
VyTran <vytran9599@gmail.com>
2019-02-14 17:36:51 -0500

    diffculty standing

---
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/entity/DifficultyStanding.java
@@ -0,0 +1,9 @@
+package edu.gatech.cs2340.spacetraders.entity;
+
+public enum DifficultyStanding {
+    FR("FR"), SO("SO"), JR("JR"), SR("SR");
+    private final String standing;
+    DifficultyStanding(String standing) {
+        this.standing = standing;
+    }
+}
