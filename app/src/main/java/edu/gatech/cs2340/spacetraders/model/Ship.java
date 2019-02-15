9f2821eb0f9a88732c11d4db5d59f2795fded092
Jianing Wang <jianingwang623@gmail.com>
2019-02-15 15:37:29 -0500

    Modified player, ship, and shipType class

--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/Ship.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/Ship.java
@@ -1,4 +1,29 @@
 package edu.gatech.cs2340.spacetraders.model;
 
 public class Ship {
+    private ShipType shipType;
+
+    /**
+     * The default constructor for ship. When the ship is initially created
+     * It has the ship type of NAT
+     */
+    public Ship() {
+        shipType = ShipType.NAT;
+    }
+
+    /**
+     * Getter method for the ship type
+     * @return the ship type of this ship
+     */
+    public ShipType getShipType() {
+        return shipType;
+    }
+
+    /**
+     * Setter method for the ship type
+     * @param newType the new ship type of this ship
+     */
+    public void setShipType(ShipType newType) {
+        shipType = newType;
+    }
 }
--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/ShipType.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/ShipType.java
@@ -1,4 +1,5 @@
 package edu.gatech.cs2340.spacetraders.model;
 
 public enum ShipType {
+    NAT;
 }
