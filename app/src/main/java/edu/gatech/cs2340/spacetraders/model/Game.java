516035deb85b0bf53d2a297fb2f50b8622fdccab
VyTran <vytran9599@gmail.com>
2019-02-15 15:52:31 -0500

    difficulty standing spinner

--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/Game.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/Game.java
@@ -1,4 +1,18 @@
 package edu.gatech.cs2340.spacetraders.model;
 
 public class Game {
+    private DifficultyStanding ds;
+    public Game(DifficultyStanding ds) {
+        this.ds = ds;
+    }
+    public void setDS(DifficultyStanding ds) {
+        this.ds = ds;
+    }
+    public DifficultyStanding getDS() {
+        return ds;
+    }
+    @Override
+    public String toString() {
+        return String.format("Difficulty standing: %s", ds);
+    }
 }
