f82eb2734015ba8f4496f16637aeb9e8cba4c47e
Jianing Wang <jianingwang623@gmail.com>
2019-02-15 15:53:16 -0500

    Modified ModelFacade class, just a little bit

--- app/src/main/java/edu/gatech/cs2340/spacetraders/model/ModelFacade.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/model/ModelFacade.java
@@ -1,12 +1,29 @@
 package edu.gatech.cs2340.spacetraders.model;
-
+/**
+ * This is the interface to the domain/business classes from anywhere else in the application
+ *
+ *
+ * This class provides the player for the application.
+ *
+ * It is a Singleton so that it can be accessed from anywhere in the application
+ */
 public class ModelFacade {
+    private Game myGame;
+    private Player myPlayer;
+    /** Singleton Pattern Code
+     *  this allows us to get access to this class
+     *  anywhere, which will allow our View models to access
+     *  the "back end"  more easily
+     */
     private static final ModelFacade ourInstance = new ModelFacade();
 
     public static ModelFacade getInstance() {
         return ourInstance;
     }
-
+    /**
+     * Make a new Model instance (should have parameters of the data entered by the player)
+     */
     private ModelFacade() {
+
     }
 }
