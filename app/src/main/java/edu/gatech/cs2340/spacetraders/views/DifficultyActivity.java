a205fa021737cfb7d95d8a085f95e1d5a54df801
VyTran <vytran9599@gmail.com>
2019-02-16 14:52:03 -0500

    change enum

---
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/views/DifficultyActivity.java
@@ -0,0 +1,19 @@
+package edu.gatech.cs2340.spacetraders.views;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import edu.gatech.cs2340.spacetraders.R;
+
+public class DifficultyActivity extends Activity implements AdapterView.OnItemSelectedListener {
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        parent.getItemAtPosition(pos);
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+}
