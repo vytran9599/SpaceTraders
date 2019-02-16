1399521a6e160bdb748a79146ded3353732c46bb
Jianing Wang <jianingwang623@gmail.com>
2019-02-16 15:13:52 -0500

    I tried to create player with the input name

--- app/src/main/java/edu/gatech/cs2340/spacetraders/viewmodels/GameViewModel.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/viewmodels/GameViewModel.java
@@ -1,4 +1,16 @@
 package edu.gatech.cs2340.spacetraders.viewmodels;
 
-public class GameViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import edu.gatech.cs2340.spacetraders.model.Player;
+
+public class GameViewModel extends AndroidViewModel {
+    public GameViewModel(@NonNull Application application) {
+        super(application);
+    }
+    public void addPlayer(String name) {
+        Player myPlayer = new Player(name, 4, 4, 4, 4);
+    }
 }
