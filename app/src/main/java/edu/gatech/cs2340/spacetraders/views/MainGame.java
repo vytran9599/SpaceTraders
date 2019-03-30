5221f64771facde93a3ae2c1cd6e73a8279c7fba
kevinbarone21 <tobybarone@gmail.com>
2019-03-30 11:01:11 -0400

    add

--- app/src/main/java/edu/gatech/cs2340/spacetraders/views/MainGame.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraders/views/MainGame.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import edu.gatech.cs2340.spacetraders.R;
 
 public class MainGame extends AppCompatActivity {
 
+    private TextView planetText, resourcesText, techText, govText, policeText, pirateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
